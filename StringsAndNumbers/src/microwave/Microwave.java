3db397df76c9366eab089adbf0baeb2edc3e978e
Beesham <sarendranauth.beesham@gmail.com>
2018-02-03 14:20:16 -0500

    Added StringsAndNUmbers proj

---
+++ StringsAndNumbers/src/microwave/Microwave.java
@@ -0,0 +1,55 @@
+package microwave;
+
+/**
+ * Class that simulates a microwave
+ */
+public class Microwave {
+	private String powerLevel;
+	private int time;
+
+	//Empty constructor
+	public Microwave() {
+	}
+
+	/**
+	 * Increases the timer by 10 seconds
+	 * @return the new time
+	 */
+	public int increaseTime() {
+		//TODO
+		return 0;
+	}
+
+	/**
+	 * Sets the power level of the microwave: Low, Medium, High
+	 * @param powerLevel
+	 * @return
+	 */
+	public String setPowerLevel(String powerLevel) {
+		//TODO
+		return null;
+	}
+
+	/**
+	 * Stops the microwave
+	 */
+	public void stop() {
+		//TODO
+	}
+
+	/**
+	 * Resets the microwave/timer to zero
+	 */
+	public void reset() {
+		//TODO
+	}
+
+	/**
+	 * Starts the timer/microwave
+	 * @return the timer and power level
+	 */
+	public String start() {
+		//TODO
+		return null;
+	}
+}
