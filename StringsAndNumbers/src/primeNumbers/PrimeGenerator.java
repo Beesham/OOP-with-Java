93d6accc074db63e4e21d53e209c4504c16a825b
Beesham <sarendranauth.beesham@gmail.com>
2018-02-04 15:58:53 -0500

    Feat: check if numbner is prime

--- StringsAndNumbers/src/primeNumbers/PrimeGenerator.java
+++ StringsAndNumbers/src/primeNumbers/PrimeGenerator.java
@@ -30,6 +30,13 @@ public class PrimeGenerator {
 	public static boolean isPrime(int n)
 	{
 		// Complete this part
-		return false;
+		if(n < 1) return false;	//Any number < 1 is not a prime
+		if(n%2 == 0) return false;	//Any number that has a factor of 2 is not prime
+
+		//Checks if 3 to n is a factor of n
+		for(int i = 3; i <= n; i++) {
+			if(n%i == 0) return false;
+		}
+		return true;
 	}
 }
