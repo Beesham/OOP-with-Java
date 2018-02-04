7a1715a33e722da55f1695058ce6b2f92ae28e82
Beesham <sarendranauth.beesham@gmail.com>
2018-02-04 15:59:13 -0500

    Add PrimePrinter class

---
+++ StringsAndNumbers/src/primeNumbers/PrimePrinter.java
@@ -0,0 +1,17 @@
+package primeNumbers;
+
+import java.util.Scanner;
+
+/**
+ * This class prints prime numbers.
+ */
+public class PrimePrinter {
+	public static void main (String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter upper limit: ");
+		int limit = in.nextInt();
+		// Complete this part
+		
+	}
+}
