519f9809863fddb6a517e3221c5b57f4cddcc29b
Beesham <sarendranauth.beesham@gmail.com>
2018-02-05 17:28:43 -0500

    Calculate sum of ever other digi | count num of those digi > 4

--- StringsAndNumbers/src/creditCardValidation/CreditCardValidator.java
+++ StringsAndNumbers/src/creditCardValidation/CreditCardValidator.java
@@ -10,17 +10,29 @@ public class CreditCardValidator {
 
 		int sum = 0;
 		for(int i = 0; i < ccNumStr.length(); i++) {
-			sum = sum + Character.getNumericValue(ccNumStr.charAt(i));
+			sum += Character.getNumericValue(ccNumStr.charAt(i));
 		}
 
 		int sumOfEverySecondDigi = 0;
+		int numOfEverySecondDigiMoreThanFour = 0;
 		for(int i = ccNumStr.length() - 1; i >=0; i--) {
-			if(i%2 == 0) sumOfEverySecondDigi = sumOfEverySecondDigi +
-					Character.getNumericValue(ccNumStr.charAt(i));
+			if(i%2 == 0) {
+				sumOfEverySecondDigi +=	Character.getNumericValue(ccNumStr.charAt(i));
+
+				if(Character.getNumericValue(ccNumStr.charAt(i)) > 4) {
+					numOfEverySecondDigiMoreThanFour++;
+				}
+			}
 		}
+
+		int finalSum = sum + sumOfEverySecondDigi + numOfEverySecondDigiMoreThanFour;
+
+		if(finalSum % 10 == 0);
 
 		System.out.println(sum);
 		System.out.println(sumOfEverySecondDigi);
+		System.out.println(numOfEverySecondDigiMoreThanFour);
+
 
 	}
 }
