3d388636631d6e59bb8b2438f40b04fc14b3f090
Beesham <sarendranauth.beesham@gmail.com>
2018-02-19 16:33:31 -0500

    Test Board | Added spaces in ticktacktoe

--- ComplexObjects/src/test/tickTackToeTest/BoardTest.java
+++ ComplexObjects/src/test/tickTackToeTest/BoardTest.java
@@ -1,4 +1,19 @@
 package test.tickTackToeTest;
 
+import tickTackToe.model.Board;
+import tickTackToe.model.Coordinate;
+
 public class BoardTest {
+	public static void main(String[] args) {
+		Board board = new Board();
+		System.out.print("Expected: blank board, Actual: ");
+		board.drawBoard();
+		System.out.println("");
+
+		board.play(new Coordinate(0,0), "X");
+		board.play(new Coordinate(2,2), "O");
+
+		System.out.print("\nExpected: X in top right, O in bottom left, Actual: ");
+		board.drawBoard();
+	}
 }
--- ComplexObjects/src/tickTackToe/model/Board.java
+++ ComplexObjects/src/tickTackToe/model/Board.java
@@ -18,13 +18,14 @@ public class Board {
 	 * Draws the game board.
 	 */
 	public void drawBoard() {
+		System.out.println('\n');
 		for(int i = 0; i < SIZE_OF_BOARD; i++) {
 			for(int j = 0; j < SIZE_OF_BOARD; j++) {
 				if(j == SIZE_OF_BOARD - 1) System.out.print(board[i][j]);
 				else System.out.print(board[i][j] + " | " );
 			}
+			if(i != SIZE_OF_BOARD - 1) System.out.println("\n----------");
 		}
-		System.out.println("----------------");
 	}
 
 	/**
