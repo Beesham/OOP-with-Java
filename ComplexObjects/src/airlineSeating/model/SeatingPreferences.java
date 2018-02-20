59f505abf1f08645a8e3ac1543cd7080c0aec3c1
Beesham <sarendranauth.beesham@gmail.com>
2018-02-20 14:34:39 -0500

    Add SeatingPref data struct

---
+++ ComplexObjects/src/airlineSeating/model/SeatingPreferences.java
@@ -0,0 +1,7 @@
+package airlineSeating.model;
+
+public class SeatingPreferences {
+	public static final String WINDOW = "window";
+	public static final String AISLE = "aisle";
+	public static final String CENTER = "center";
+}
