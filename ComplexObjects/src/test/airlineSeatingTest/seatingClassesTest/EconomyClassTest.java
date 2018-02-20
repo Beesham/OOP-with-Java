fab838c7435756a7e71ed45b001e8d565d6745b7
Beesham <sarendranauth.beesham@gmail.com>
2018-02-20 16:36:55 -0500

    Add new test case

--- ComplexObjects/src/test/airlineSeatingTest/seatingClassesTest/EconomyClassTest.java
+++ ComplexObjects/src/test/airlineSeatingTest/seatingClassesTest/EconomyClassTest.java
@@ -5,12 +5,35 @@ import airlineSeating.model.Passenger;
 import airlineSeating.model.Seat;
 import airlineSeating.model.SeatingPreferences;
 
+import java.util.List;
+
 /**
  * Test EconomyClass
  * @author Beesham Sarendranauth
  */
 public class EconomyClassTest {
 	public static void main(String[] args) {
+		testMultiPassenger();
+	}
+
+	public static void testMultiPassenger() {
+		EconomyClass economyClass = new EconomyClass(new Passenger[EconomyClass.MAX_ROWS_ECON][EconomyClass.MAX_COLS_ECON]);
+
+		Passenger pW = new Passenger(economyClass, SeatingPreferences.WINDOW);
+		Passenger pC = new Passenger(economyClass, SeatingPreferences.CENTER);
+		Passenger pA = new Passenger(economyClass, SeatingPreferences.AISLE);
+
+		economyClass.assignSeat(pW, new Seat(0,0));
+
+		try {
+			List<Seat> seatList = economyClass.findMatch(3);
+			System.out.println(seatList.toString());
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public static void testSinglePassenger() {
 		EconomyClass economyClass = new EconomyClass(new Passenger[EconomyClass.MAX_ROWS_ECON][EconomyClass.MAX_COLS_ECON]);
 
 		Passenger pW = new Passenger(economyClass, SeatingPreferences.WINDOW);
