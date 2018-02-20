78ca1c71ad18a0a4575a1e74ea6a6005b55d804b
Beesham <sarendranauth.beesham@gmail.com>
2018-02-20 14:27:24 -0500

    Add seat object to airlineseating

---
+++ ComplexObjects/src/airlineSeating/model/Seat.java
@@ -0,0 +1,23 @@
+package airlineSeating.model;
+
+/**
+ * Models a seat (row, column)
+ * @author Beesham Sarendranauth
+ */
+public class Seat {
+	private int row;
+	private int column;
+
+	public Seat(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+}
