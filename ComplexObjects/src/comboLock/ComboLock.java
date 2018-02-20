cc0c43407940e88d3f770c5e319b70f1ffafcb0d
Beesham <sarendranauth.beesham@gmail.com>
2018-02-20 10:55:08 -0500

    Impl funcs in ComboLock

--- ComplexObjects/src/comboLock/ComboLock.java
+++ ComplexObjects/src/comboLock/ComboLock.java
@@ -9,12 +9,27 @@ public class ComboLock {
 	public static final int MIN_COMBO_NUM = 0;
 	public static final int MAX_COMBO_NUM = 39;
 
+	/*
+	The combo to open the lock
+	 */
 	private int secret1, secret2, secret3;
+
+	/*
+	The numbers chosen by the user
+	 */
+	private int num1, num2, num3;
+
+	/*
+	Counts how many times the dial was touched
+	 */
+	private int dialTouchCount;
 
 	public ComboLock(int secret1, int secret2, int secret3) {
 		this.secret1 = secret1;
 		this.secret2 = secret2;
 		this.secret3 = secret3;
+
+		dialTouchCount = 0;
 	}
 
 	/**
@@ -26,19 +41,34 @@ public class ComboLock {
 	 * Turns the dial left a given number of times
 	 * @param ticks the number of times to turn the dial
 	 */
-	public void turnLeft(int ticks) { }
+	public void turnLeft(int ticks) {
+		if(dialTouchCount == 1) {
+			num2 = ticks;
+		}
+	}
 
 	/**
 	 * Turns the dial right a given number of times
 	 * @param ticks the number of times to turn the dial
 	 */
-	public void turnRight(int ticks) {  }
+	public void turnRight(int ticks) {
+		if(dialTouchCount == 0) {
+			num1 = ticks;
+		}else if(dialTouchCount == 2) {
+			num3 = ticks;
+		}
+	}
 
 	/**
 	 * Attempts to open lock
 	 * @return true if combo is correct, else false
 	 */
 	public boolean open() {
+		if(num1 == secret1 &&
+				num2 == secret2 &&
+				num3 == secret3) {
+			return true;
+		}
 		return false;
 	}
 }
