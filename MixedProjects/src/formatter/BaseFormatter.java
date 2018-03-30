c0688f86b34b59c10dbd31f31c727d1a114dc46c
Beesham <sarendranauth.beesham@gmail.com>
2018-03-30 18:35:39 -0400

    Implement base formatting - format project

--- MixedProjects/src/formatter/BaseFormatter.java
+++ MixedProjects/src/formatter/BaseFormatter.java
@@ -5,8 +5,18 @@ package formatter;
  * Formats a number by base eg.base 2 or octal
  */
 public class BaseFormatter implements NumberFormatter {
+
+	/*
+		The base to convert input number to
+	 */
+	int base;
+
+	public BaseFormatter(int base) {
+		this.base = base;
+	}
+
 	@Override
 	public String format(int n) {
-		return null;
+		return Integer.toString(n, base);
 	}
 }
