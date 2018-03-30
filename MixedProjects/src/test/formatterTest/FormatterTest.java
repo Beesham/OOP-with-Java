12709fc05d7b4fb753d4ee9a4b6963c8b3e03ef2
Beesham <sarendranauth.beesham@gmail.com>
2018-03-30 18:36:03 -0400

    Add test cases - format project

--- MixedProjects/src/test/formatterTest/FormatterTest.java
+++ MixedProjects/src/test/formatterTest/FormatterTest.java
@@ -1,8 +1,12 @@
 package test.formatterTest;
 
 import formatter.AccountingFormatter;
+import formatter.BaseFormatter;
 import formatter.DecimalSeparatorFormatter;
 import formatter.DefaultFormatter;
+
+import java.io.*;
+import java.util.Scanner;
 
 /**
  * @author Beesham Sarendranauth
@@ -17,6 +21,23 @@ public class FormatterTest {
 
 		AccountingFormatter accountingFormatter = new AccountingFormatter();
 		System.out.println("Expected: (5) Actual: " + accountingFormatter.format(-5));
+
+		BaseFormatter baseFormatter = new BaseFormatter(2);
+		System.out.println("Expected: 101 Actual: " + baseFormatter.format(5));
+		System.out.println("Expected: -101 Actual: " + baseFormatter.format(-5));
+
+		try {
+			File inFile = new File("Numbers.txt");
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("FormattedNumbers.txt", true)));
+			Scanner in = new Scanner(inFile);
+			while (in.hasNextInt()) {
+				String s = baseFormatter.format(in.nextInt());
+				out.append(s + " ");
+			}
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
