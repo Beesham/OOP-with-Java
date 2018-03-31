f7b88954da283f5e3c2365030b02a0bdcf847caf
Beesham <sarendranauth.beesham@gmail.com>
2018-03-30 23:41:12 -0400

    Draw four houses - house

--- MixedProjects/src/graphicalHouse/HomeViewer.java
+++ MixedProjects/src/graphicalHouse/HomeViewer.java
@@ -1,6 +1,7 @@
 package graphicalHouse;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author Beesham Sarendranauth
@@ -9,11 +10,19 @@ import javax.swing.*;
 public class HomeViewer {
 	public static void main(String[] args) {
 
+		GridLayout gridLayout = new GridLayout(0, 2);
+
 		JFrame frame = new JFrame();
 		frame.setSize(500,500);
 		frame.setTitle("Houses");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.getContentPane().setLayout(gridLayout);
 		frame.add(new HomeComponent());
+		frame.add(new HomeComponent());
+		frame.add(new HomeComponent());
+		frame.add(new HomeComponent());
+
 		frame.setVisible(true);
 
 	}
