8dadc82f3e21b911d013bd844bc125494e8910f0
Beesham <sarendranauth.beesham@gmail.com>
2018-03-30 23:47:59 -0400

    Add new subproject - csvReader

---
+++ MixedProjects/src/csvReader/CSVReader.java
@@ -0,0 +1,37 @@
+package csvReader;
+
+/**
+ * @author Beesham Sarendranauth
+ * Reads a csv file
+ */
+public class CSVReader {
+
+	/**
+	 * Counts the number of lines in the CSV file
+	 * @return the number of lines
+	 */
+	public int numberOfRows() {
+		return 0;
+	}
+
+	/**
+	 * Counts the number of fields in a particular row
+	 * @param row the row to count
+	 * @return the number of fields
+	 */
+	public int numberOfFields(int row) {
+
+		return 0;
+	}
+
+	/**
+	 * Reads the field in a particular row and column
+	 * @param row row to read
+	 * @param column the column to read
+	 * @return the content of the field read
+	 */
+	public String field(int row, int column) {
+
+		return null;
+	}
+}
